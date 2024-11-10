38cc0490fa48828d4b269ab37718f270de8c7b82
Vishal Chaure <vishal.chaure22@spit.ac.in>
2024-11-10 23:59:58 +0530

    first commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,8 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = 'http://localhost:4000'
+  // const url = 'http://localhost:4000'
+  const url = 'https://food-delivery-website-z986.vercel.app'
   return (
     <div>
       <ToastContainer/>
